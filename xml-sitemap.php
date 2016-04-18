4ac125405fb3545434a80130a7f92d03d9d7d079
RavanH <ravanhagen@gmail.com>
2016-04-19 00:26:38 +0200

    db version update

--- xml-sitemap.php
+++ xml-sitemap.php
@@ -50,7 +50,7 @@ if ( ! defined( 'WPINC' ) ) die;
  *      CONSTANTS
  * -------------------- */
 
-	define('XMLSF_VERSION', '4.6.1');
+	define('XMLSF_VERSION', '4.6.3');
 
 	define('XMLSF_PLUGIN_BASENAME', plugin_basename(__FILE__));
 
