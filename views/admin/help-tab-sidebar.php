48acb4e9bc9b0bc2475e8181e1cd52e112dbff36
RavanH <ravanhagen@gmail.com>
2018-09-08 23:10:30 +0200

    translator hints

--- views/admin/help-tab-sidebar.php
+++ views/admin/help-tab-sidebar.php
@@ -2,8 +2,8 @@
 	<strong><?php echo translate('For more information:'); ?></strong>
 </p>
 <p>
-	<a href="https://support.google.com/webmasters/answer/156184" target="_blank"><?php _e( 'Learn more about sitemaps on Google Search Console Help', 'xml-sitemap-feed' ); ?></a>
+	<a href="https://support.google.com/webmasters/answer/156184" target="_blank"><?php _e( /* https://support.google.com/webmasters/answer/156184 */ 'Learn more about sitemaps on Google Search Console Help', 'xml-sitemap-feed' ); ?></a>
 </p>
 <p>
-	<a href="https://moz.com/blog/xml-sitemaps" target="_blank"><?php _e( 'XML Sitemaps: The Most Misunderstood Tool in the SEO\'s Toolbox', 'xml-sitemap-feed' ); ?></a>
+	<a href="https://moz.com/blog/xml-sitemaps" target="_blank"><?php _e( /* https://moz.com/blog/xml-sitemaps */ 'XML Sitemaps: The Most Misunderstood Tool in the SEO\'s Toolbox', 'xml-sitemap-feed' ); ?></a>
 </p>
