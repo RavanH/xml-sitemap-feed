ee2c66eb0d759fbac495b3daedf4cd728ffb5edb
RavanH <ravanhagen@gmail.com>
2018-11-05 10:53:35 +0100

    remove factcheck text string from admin help tab

--- views/admin/help-tab-news-labels.php
+++ views/admin/help-tab-news-labels.php
@@ -3,6 +3,5 @@
 </p>
 <p>
 	<?php printf(__('Source labels inside a News Sitemap are no longer supported by Google News. To manage your site\'s labels, please go to the %s.','xml-sitemap-feed'),'<a href="https://news.google.com/publisher" target="_blank">'.__('Google News Publisher Center','xml-sitemap-feed').'</a>'); ?>
-	<?php _e('The FactCheck label may only be applied if you publish stories with fact-checking content that\'s indicated by schema.org ClaimReview markup.','xml-sitemap-feed'); ?>
 	<?php printf(__('Read more about source labels on %s','xml-sitemap-feed'),'<a href="https://support.google.com/news/publisher/answer/4582731" target="_blank">'.__('What does each source label mean?','xml-sitemap-feed').'</a>'); ?>
 </p>
