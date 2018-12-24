edcba30d4adfe68251b27dc0736b009b2448f753
RavanH <ravanhagen@gmail.com>
2018-12-24 02:57:03 +0100

    robots help tab links

--- views/admin/help-tab-robots.php
+++ views/admin/help-tab-robots.php
@@ -5,3 +5,9 @@
 		<?php _e('Only add rules here when you know what you are doing, otherwise you might break search engine access to your site.','xml-sitemap-feed'); ?>
 	</span>
 </p>
+<p>
+	<a href="https://www.google.com/webmasters/tools/robots-testing-tool" target="_blank" class="button"><?php _e('Open robots.txt Tester','xml-sitemap-feed'); ?></a>
+</p>
+<p>
+	<?php printf( __('For more help see %s and %s.','xml-sitemap-feed'), '<a href="https://developers.google.com/search/reference/robots_txt" target="_blank">'.__('Learn about robots.txt files','xml-sitemap-feed').'</a>','<a href="https://developers.google.com/search/reference/robots_txt" target="_blank">'.__('Robots.txt Specifications','xml-sitemap-feed').'</a>' ); ?>
+</p>
