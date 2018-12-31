c126eea21be82a0ca0023f3f9b783d2719ccd3ea
RavanH <ravanhagen@gmail.com>
2018-12-31 04:15:46 +0100

    additional help links

--- views/admin/help-tab-news-sidebar.php
+++ views/admin/help-tab-news-sidebar.php
@@ -1,12 +1,23 @@
 <p>
 	<strong><?php echo translate('For more information:'); ?></strong>
 </p>
-<p>
-	<a href="https://support.google.com/webmasters/answer/183669" target="_blank"><?php _e( /* https://support.google.com/webmasters/answer/183669 */ 'Manage sitemaps with the Sitemaps report', 'xml-sitemap-feed' ); ?></a>
-</p>
-<p>
-	<a href="https://support.google.com/news/publisher/answer/13369" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/answer/13369 */ 'Prevent missing or incorrect images', 'xml-sitemap-feed' ); ?></a>
-</p>
-<p>
-	<a href="https://support.google.com/news/publisher-center/topic/9010385" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/topic/9010385 */ 'Troubleshoot an issue', 'xml-sitemap-feed' ); ?></a>
-</p>
+<ul>
+	<li>
+		<a href="https://support.google.com/news/publisher-center/answer/74288" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/answer/74288 */ 'Create a Google News sitemap', 'xml-sitemap-feed' ); ?></a>
+	</li>
+	<li>
+		<a href="https://support.google.com/news/publisher-center/answer/184732" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/answer/184732 */ 'Validate a News sitemap', 'xml-sitemap-feed' ); ?></a>
+	</li>
+	<li>
+		<a href="https://support.google.com/news/publisher-center/answer/6075793" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/answer/6075793 */ 'News sitemap errors', 'xml-sitemap-feed' ); ?></a>
+	</li>
+	<li>
+		<a href="https://support.google.com/news/publisher-center/topic/9010385" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/topic/9010385 */ 'Troubleshoot an issue', 'xml-sitemap-feed' ); ?></a>
+	</li>
+	<li>
+		<a href="https://support.google.com/news/publisher-center/answer/13369" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/answer/13369 */ 'Prevent missing or incorrect images', 'xml-sitemap-feed' ); ?></a>
+	</li>
+	<li>
+		<a href="https://support.google.com/news/publisher-center/answer/70871" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/answer/70871 */ 'Incorrect article date', 'xml-sitemap-feed' ); ?></a>
+	</li>
+</ul>
--- views/admin/help-tab-sidebar.php
+++ views/admin/help-tab-sidebar.php
@@ -1,9 +1,14 @@
 <p>
 	<strong><?php echo translate('For more information:'); ?></strong>
 </p>
-<p>
-	<a href="https://support.google.com/webmasters/answer/156184" target="_blank"><?php _e( /* https://support.google.com/webmasters/answer/156184 */ 'Learn more about sitemaps on Google Search Console Help', 'xml-sitemap-feed' ); ?></a>
-</p>
-<p>
-	<a href="https://moz.com/blog/xml-sitemaps" target="_blank"><?php _e( /* https://moz.com/blog/xml-sitemaps */ 'XML Sitemaps: The Most Misunderstood Tool in the SEO\'s Toolbox', 'xml-sitemap-feed' ); ?></a>
-</p>
+<ul>
+	<li>
+		<a href="https://support.google.com/webmasters/answer/156184" target="_blank"><?php _e( /* https://support.google.com/webmasters/answer/156184 */ 'Learn more about sitemaps on Google Search Console Help', 'xml-sitemap-feed' ); ?></a>
+	</li>
+	<li>
+		<a href="https://support.google.com/webmasters/answer/183669" target="_blank"><?php _e( /* https://support.google.com/webmasters/answer/183669 */ 'Manage sitemaps with the Sitemaps report', 'xml-sitemap-feed' ); ?></a>
+	</li>
+	<li>
+		<a href="https://moz.com/blog/xml-sitemaps" target="_blank"><?php _e( /* https://moz.com/blog/xml-sitemaps */ 'XML Sitemaps: The Most Misunderstood Tool in the SEO\'s Toolbox', 'xml-sitemap-feed' ); ?></a>
+	</li>
+</ul>
