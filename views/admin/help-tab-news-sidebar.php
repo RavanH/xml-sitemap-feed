1ec202350e2f11f1b17f5c37ba93e7f237a9894a
RavanH <ravanhagen@gmail.com>
2019-10-19 03:20:51 +0200

    xml validation links

--- views/admin/help-tab-news-sidebar.php
+++ views/admin/help-tab-news-sidebar.php
@@ -2,11 +2,11 @@
 	<strong><?php echo translate('For more information:'); ?></strong>
 </p>
 <ul>
-	<li>
+	<!--<li>
 		<a href="https://support.google.com/news/publisher-center/answer/74288" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/answer/74288 */ 'Create a Google News sitemap', 'xml-sitemap-feed' ); ?></a>
-	</li>
+	</li>-->
 	<li>
-		<a href="https://support.google.com/news/publisher-center/answer/184732" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/answer/184732 */ 'Validate a News sitemap', 'xml-sitemap-feed' ); ?></a>
+		<a href="https://www.xml-sitemaps.com/validate-xml-sitemap.html" target="_blank"><?php _e( /* https://www.xml-sitemaps.com/validate-xml-sitemap.html */ 'Validate an XML Sitemap', 'xml-sitemap-feed' ); ?></a>
 	</li>
 	<li>
 		<a href="https://support.google.com/news/publisher-center/answer/6075793" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/answer/6075793 */ 'News sitemap errors', 'xml-sitemap-feed' ); ?></a>
