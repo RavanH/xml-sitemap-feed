82be8719d9685c9c2f780e069a5ed874f6ecbc30
RavanH <ravanhagen@gmail.com>
2019-10-19 03:29:50 +0200

    more help links

--- views/admin/help-tab-news-sidebar.php
+++ views/admin/help-tab-news-sidebar.php
@@ -6,6 +6,12 @@
 		<a href="https://support.google.com/news/publisher-center/answer/74288" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/answer/74288 */ 'Create a Google News sitemap', 'xml-sitemap-feed' ); ?></a>
 	</li>-->
 	<li>
+		<a href="https://support.google.com/news/publisher/answer/40787" target="_blank"><?php _e( /* https://support.google.com/news/publisher/answer/40787 */ 'Getting into Google News', 'xml-sitemap-feed' ); ?></a>
+	</li>
+	<li>
+		<a href="https://partnerdash.google.com/partnerdash/d/news#p:id=pfehome"><?php _e( /* https://partnerdash.google.com/partnerdash/d/news#p:id=pfehome */ 'Google News Publisher Center', 'xml-sitemap-feed' ); ?></a>
+	</li>
+	<li>
 		<a href="https://www.xml-sitemaps.com/validate-xml-sitemap.html" target="_blank"><?php _e( /* https://www.xml-sitemaps.com/validate-xml-sitemap.html */ 'Validate an XML Sitemap', 'xml-sitemap-feed' ); ?></a>
 	</li>
 	<li>
@@ -19,5 +25,8 @@
 	</li>
 	<li>
 		<a href="https://support.google.com/news/publisher-center/answer/70871" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/answer/70871 */ 'Incorrect article date', 'xml-sitemap-feed' ); ?></a>
+	</li>
+	<li>
+		<a href="https://groups.google.com/a/googleproductforums.com/forum/#!categories/news/google-news-publishers" target="_blank"><?php _e( /* https://groups.google.com/a/googleproductforums.com/forum/#!categories/news/google-news-publishers */ 'Publisher Help Forum', 'xml-sitemap-feed' ); ?></a>
 	</li>
 </ul>
