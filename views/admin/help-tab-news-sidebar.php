35fd6e891a5a9b0ba6ef270423f4a280367c0bcc
RavanH <ravanhagen@gmail.com>
2019-11-16 16:33:50 +0100

    help links

--- views/admin/help-tab-news-sidebar.php
+++ views/admin/help-tab-news-sidebar.php
@@ -3,27 +3,24 @@
 </p>
 <ul>
 	<!--<li>
-		<a href="https://support.google.com/news/publisher-center/answer/74288" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/answer/74288 */ 'Create a Google News sitemap', 'xml-sitemap-feed' ); ?></a>
+		<a href="https://support.google.com/news/publisher-center/answer/74288" target="_blank"><?php _e( /* Translators: Article title https://support.google.com/news/publisher-center/answer/74288 */ 'Create a Google News sitemap', 'xml-sitemap-feed' ); ?></a>
 	</li>-->
 	<li>
-		<a href="https://support.google.com/news/publisher/answer/40787" target="_blank"><?php _e( /* https://support.google.com/news/publisher/answer/40787 */ 'Getting into Google News', 'xml-sitemap-feed' ); ?></a>
+		<a href="https://support.google.com/news/publisher/answer/40787" target="_blank"><?php _e( /* Translators: Article title https://support.google.com/news/publisher/answer/40787 */ 'Getting into Google News', 'xml-sitemap-feed' ); ?></a>
 	</li>
 	<li>
-		<a href="https://support.google.com/news/publisher-center/answer/6075793" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/answer/6075793 */ 'News sitemap errors', 'xml-sitemap-feed' ); ?></a>
+		<a href="https://webmasters.googleblog.com/2019/01/ways-to-succeed-in-google-news.html" target="_blank"><?php _e( /* Translators: Article title https://webmasters.googleblog.com/2019/01/ways-to-succeed-in-google-news.html */ 'Ways to succeed in Google News', 'xml-sitemap-feed' ); ?></a>
 	</li>
 	<li>
-		<a href="https://support.google.com/news/publisher-center/topic/9010385" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/topic/9010385 */ 'Troubleshoot an issue', 'xml-sitemap-feed' ); ?></a>
+		<a href="https://support.google.com/news/publisher-center/answer/6075793" target="_blank"><?php _e( /* Translators: Article title https://support.google.com/news/publisher-center/answer/6075793 */ 'News sitemap errors', 'xml-sitemap-feed' ); ?></a>
 	</li>
 	<li>
-		<a href="https://support.google.com/news/publisher-center/answer/13369" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/answer/13369 */ 'Prevent missing or incorrect images', 'xml-sitemap-feed' ); ?></a>
+		<a href="https://support.google.com/news/publisher-center/topic/9010385" target="_blank"><?php _e( /* Translators: Article title https://support.google.com/news/publisher-center/topic/9010385 */ 'Troubleshoot an issue', 'xml-sitemap-feed' ); ?></a>
 	</li>
 	<li>
-		<a href="https://support.google.com/news/publisher-center/answer/70871" target="_blank"><?php _e( /* https://support.google.com/news/publisher-center/answer/70871 */ 'Incorrect article date', 'xml-sitemap-feed' ); ?></a>
+		<a href="https://support.google.com/news/publisher-center/answer/13369" target="_blank"><?php _e( /* Translators: Article title https://support.google.com/news/publisher-center/answer/13369 */ 'Prevent missing or incorrect images', 'xml-sitemap-feed' ); ?></a>
 	</li>
 	<li>
-		<a href="https://support.google.com/googlenews/" target="_blank"><?php _e( /* https://support.google.com/googlenews/ */ 'Google News Help Center', 'xml-sitemap-feed' ); ?></a>
-	</li>
-	<li>
-		<a href="https://support.google.com/googlenews/community" target="_blank"><?php _e( /* https://support.google.com/googlenews/community */ 'Publisher Help Forum', 'xml-sitemap-feed' ); ?></a>
+		<a href="https://support.google.com/news/publisher-center/answer/70871" target="_blank"><?php _e( /* Translators: Article title https://support.google.com/news/publisher-center/answer/70871 */ 'Incorrect article date', 'xml-sitemap-feed' ); ?></a>
 	</li>
 </ul>
--- views/admin/sidebar-help.php
+++ views/admin/sidebar-help.php
@@ -1,8 +1,14 @@
 <h3><span class="dashicons dashicons-sos"></span> <?php echo translate('Help'); ?></h3>
 <p>
 	<?php printf (
-	/* translators: Support forum URL on WordPress.org */
-	__( 'You can find instructions on the help tab above. If you still have questions, please go to the <a href="%s" target="_blank">Support forum</a>.', 'xml-sitemap-feed' ),
-	'https://wordpress.org/support/plugin/xml-sitemap-feed'
+		/* translators: Support forum URL on WordPress.org */
+		__( 'You can find instructions on the help tab above. If you still have questions, please go to the <a href="%s" target="_blank">Support forum</a>.', 'xml-sitemap-feed' ),
+		'https://wordpress.org/support/plugin/xml-sitemap-feed'
+	); ?>
+	<?php printf (
+		/* translators: links to Google News Help Center and Publisher Help Forum */
+		__( 'More general help can be found on %1$s and %2$s.', 'xml-sitemap-feed' ),
+		'<a href="https://support.google.com/googlenews/" target="_blank">' . __( /* translators: Site title https://support.google.com/googlenews/ */ 'Google News Help Center', 'xml-sitemap-feed' ) . '</a>',
+		'<a href="https://support.google.com/googlenews/community" target="_blank">' . __( /* Translators: Forum title https://support.google.com/googlenews/community */ 'Publisher Help Forum', 'xml-sitemap-feed' ) . '</a>'
 	); ?>
 </p>
