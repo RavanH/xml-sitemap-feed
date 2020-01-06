9210604e0f06d0d44ba623a7a6aabb1f81eaf6ac
RavanH <ravanhagen@gmail.com>
2020-01-07 00:40:40 +0100

    add query errors to debug info

--- views/_usage.php
+++ views/_usage.php
@@ -1,2 +1,3 @@
 <!-- Queries executed: <?php echo $num; if ( $mem ) { ?> | Peak memory usage: <?php echo $mem; } ?> | Memory limit: <?php echo $limit; ?> -->
-<!-- <?php if ( defined('SAVEQUERIES') && SAVEQUERIES == true ) { echo 'Queries: '.PHP_EOL; print_r($wpdb->queries); } else { echo 'Set SAVEQUERIES or run Query Monitor to show saved database queries here.'; } ?> -->
+<!-- Query errors: <?php echo !empty($errors) ? $errors : 'None encountered.'; ?> -->
+<!-- Queries: <?php  echo !empty($saved) ? $saved : 'Set SAVEQUERIES to show saved database queries here.'; ?> -->
