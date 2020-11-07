b4599899d134f920a7095861ac9e4ce3e4ed0366
RavanH <ravanhagen@gmail.com>
2020-11-07 17:29:52 +0100

    nicer message

--- views/_usage.php
+++ views/_usage.php
@@ -1,4 +1,4 @@
-<!-- Queries executed: <?php echo $num; if ( $mem ) { ?> | Peak memory usage: <?php echo $mem; } ?> | Memory limit: <?php echo $limit; ?> -->
+<!-- Queries executed: <?php echo $num; if ( $mem ) { ?> | Peak memory usage: <?php echo $mem ? $mem : 'Not availabe.'; } ?> | Memory limit: <?php echo $limit; ?> -->
 <!-- Query errors: <?php echo !empty($errors) ? $errors : 'None encountered.'; ?> -->
 <!-- Queries: <?php echo !empty($saved) ? $saved : 'Set SAVEQUERIES to show saved database queries here.'; ?> -->
 <!-- Average system load during the last minute: <?php $load = function_exists('sys_getloadavg') ? sys_getloadavg() : false; echo $load ? $load[0] : 'Not available.'; ?> -->
