2e9a6795cab56fed8f74c446ad9a09b3f368bc22
RavanH <ravanhagen@gmail.com>
2024-01-17 23:37:09 +0100

    warning on unsaved changes

--- views/admin/page-sitemap.php
+++ views/admin/page-sitemap.php
@@ -28,7 +28,7 @@
 	</nav>
 
 	<div class="main">
-		<form method="post" action="options.php">
+		<form id="xmlsf-settings-form" method="post" action="options.php">
 
 			<?php settings_fields( 'xmlsf_' . $active_tab ); ?>
 
@@ -82,3 +82,21 @@
 	</div>
 
 </div>
+<script>
+document.addEventListener('DOMContentLoaded', function () {
+	var mainform, isSubmitting = false;
+	jQuery(document).ready(function () {
+		mainform = jQuery('#xmlsf-settings-form');
+		mainform.submit(function(){
+			isSubmitting = true
+		})
+		mainform.data('initial-state', mainform.serialize());
+		jQuery(window).on('beforeunload', function(event) {
+			if (!isSubmitting && mainform.length && mainform.serialize() != mainform.data('initial-state')){
+				event.preventDefault();
+				return "<?php esc_html_e( 'The changes you made will be lost if you navigate away from this page.' ) ?>";
+			}
+		});
+	});
+}, false );
+</script>
