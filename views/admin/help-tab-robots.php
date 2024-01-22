ef19cd4f6abb31b4b078314717b8ecf3c186a6fd
RavanH <ravanhagen@gmail.com>
2024-01-22 18:11:26 +0100

    robots.txt testing tool links

--- views/admin/help-tab-robots.php
+++ views/admin/help-tab-robots.php
@@ -14,7 +14,16 @@
 	</span>
 </p>
 <p>
-	<a href="https://search.google.com/search-console/settings/robots-txt?resource_id=sc-domain%3A<?php echo esc_url_raw( wp_parse_url( get_bloginfo( 'url' ), PHP_URL_HOST ) ); ?>" target="_blank" class="button"><?php esc_html_e( 'Open robots.txt Tester', 'xml-sitemap-feed' ); ?></a>
+	<strong>
+	<?php
+	printf( /* translators: %1$s Google Search Console, %2$s Bing Webmaster Tools, %3$s Yandex Webmaster (all presented as buttons, opening in new tab) */
+		esc_html( 'Verify your robots.txt on %1$s %2$s %3$s' ),
+		'<a href="https://search.google.com/search-console/settings/robots-txt" target="_blank" class="button">' . esc_html__( 'Google Search Console', 'xml-sitemap-feed' ) . '</a>',
+		'<a href="https://www.bing.com/webmasters/robotstxttester" target="_blank" class="button">' . esc_html__( 'Bing Webmaster Tools', 'xml-sitemap-feed' ) . '</a>',
+		'<a href="https://webmaster.yandex.com/tools/robotstxt/" target="_blank" class="button">' . esc_html__( 'Yandex Webmaster', 'xml-sitemap-feed' ) . '</a>'
+	);
+	?>
+	</strong>
 </p>
 <p>
 	<?php printf( /* translators: Learn about robots.txt (linked to https://developers.google.com/search/docs/crawling-indexing/robots/intro), Robots.txt Specification (linked to https://developers.google.com/search/docs/crawling-indexing/robots/robots_txt) */ esc_html__( 'For more help see %1$s and %2$s.', 'xml-sitemap-feed' ), '<a href="https://developers.google.com/search/docs/crawling-indexing/robots/intro" target="_blank">' . esc_html__( 'Learn about robots.txt files', 'xml-sitemap-feed' ) . '</a>', '<a href="https://developers.google.com/search/docs/crawling-indexing/robots/robots_txt" target="_blank">' . esc_html__( 'Robots.txt Specifications', 'xml-sitemap-feed' ) . '</a>' ); ?>
