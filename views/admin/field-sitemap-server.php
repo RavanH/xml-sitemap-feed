f394c7e4413a869b580d236f6505783b74cf6404
RavanH <ravanhagen@gmail.com>
2024-02-20 19:05:36 +0100

    translation text and comment

--- views/admin/field-sitemap-server.php
+++ views/admin/field-sitemap-server.php
@@ -28,7 +28,7 @@
 		?>
 	<p class="description">
 		<?php
-		printf( /* translators: Site Health admin page, linked */ esc_html__( 'It appears the SimpleXML module is not available. Please use the alternative XML sitemap server or install the missing PHP module. See recommendations on %s.', 'xml-sitemap-feed' ), '<a href="' . esc_url( admin_url( 'site-health.php' ) ) . '">' . esc_html( translate( 'Site Health' ) ) . '</a>' );
+		printf( /* translators: Site Health admin page, linked */ esc_html__( 'It appears the SimpleXML module is not available. Please use the Plugin\'s XML sitemap server or install the missing PHP module. See recommendations on %s.', 'xml-sitemap-feed' ), '<a href="' . esc_url( admin_url( 'site-health.php' ) ) . '">' . esc_html( translate( 'Site Health' ) ) . '</a>' );
 		?>
 	</p>
 		<?php
@@ -37,7 +37,7 @@
 		?>
 	<p class="description">
 		<?php
-		printf( /* translators: Site Health admin page, linked */ esc_html__( 'It appears the WordPress core server is not available. Please use the alternative XML sitemap server or upgrade to %s.', 'xml-sitemap-feed' ), '<a href="https://wordpress.org/download/releases/" target="_blank">' . esc_html__( 'WordPress version 5.5+', 'xml-sitemap-feed' ) . '</a>' );
+		printf( /* translators: WordPress Downloads page, linked */ esc_html__( 'It appears the WordPress core server is not available. Please use the Plugin\'s XML sitemap server or upgrade to %s.', 'xml-sitemap-feed' ), '<a href="https://wordpress.org/download/releases/" target="_blank">' . esc_html__( 'WordPress version 5.5+', 'xml-sitemap-feed' ) . '</a>' );
 		?>
 	</p>
 		<?php
