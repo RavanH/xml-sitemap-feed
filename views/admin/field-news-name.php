644cdfe5055800143be6099ce8abe634d820ad7e
RavanH <ravanhagen@gmail.com>
2024-02-22 00:33:27 +0100

    publication name placeholder

--- views/admin/field-news-name.php
+++ views/admin/field-news-name.php
@@ -8,7 +8,7 @@
 ?>
 <fieldset>
 	<legend class="screen-reader-text"><?php esc_html_e( 'Publication name', 'xml-sitemap-feed' ); ?></legend>
-	<input type="text" name="xmlsf_news_tags[name]" id="xmlsf_news_name" value="<?php echo esc_attr( $name ); ?>" class="regular-text">
+	<input type="text" name="xmlsf_news_tags[name]" id="xmlsf_news_name" value="<?php echo esc_attr( $name ); ?>" class="regular-text" placeholder="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
 	<p class="description">
 		<?php printf( /* translators: Site Title linked to Options > General */ esc_html__( 'By default, the general %s setting will be used.', 'xml-sitemap-feed' ), '<a href="options-general.php">' . esc_html__( 'Site Title' ) . '</a>' ); ?>
 	</p>
