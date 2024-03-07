7977c195a196f6927b297c13b051aaa64f953316
RavanH <ravanhagen@gmail.com>
2024-03-07 16:41:53 +0100

    polylang compat

--- inc/functions-compat-polylang.php
+++ inc/functions-compat-polylang.php
@@ -23,7 +23,7 @@ function xmlsf_polylang_request( $request ) {
 \add_filter( 'xmlsf_core_request', 'xmlsf_polylang_request' );
 \add_filter( 'xmlsf_news_request', 'xmlsf_polylang_request' );
 
-// Remove Polylang filters.
+// Remove Polylang filters to place all languages in the same sitemaps.
 \remove_filter( 'pll_set_language_from_query', array( $polylang->sitemaps, 'set_language_from_query' ) );
 \remove_filter( 'rewrite_rules_array', array( $polylang->sitemaps, 'rewrite_rules' ) );
 \remove_filter( 'wp_sitemaps_add_provider', array( $polylang->sitemaps, 'replace_provider' ) );
@@ -35,3 +35,17 @@ add_action(
 		\add_filter( 'pll_check_canonical_url', '__return_false' );
 	}
 );
+
+/**
+ * Post language filter for Polylang.
+ *
+ * @param string $locale Locale.
+ * @param int    $post_id Post ID.
+ *
+ * @return string
+ */
+function polylang_post_language_filter( $locale, $post_id ) {
+	return \function_exists( 'pll_get_post_language' ) ? \pll_get_post_language( $post_id, 'locale' ) : $locale;
+}
+
+//\add_filter( 'xmlsf_news_language', __NAMESPACE__ . '\polylang_post_language_filter', 10, 2 );
