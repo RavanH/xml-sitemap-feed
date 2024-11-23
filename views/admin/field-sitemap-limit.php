bbdd04a957fa3b57bc4894e675267b9011b9b1c2
RavanH <ravanhagen@gmail.com>
2024-11-23 12:09:37 +0100

    Change translation string

--- views/admin/field-sitemap-limit.php
+++ views/admin/field-sitemap-limit.php
@@ -13,8 +13,8 @@
 
 	<p>
 		<label>
+			<?php esc_html_e( 'Maximum posts per sitemap', 'xml-sitemap-feed' ); ?>
 			<input type="number" step="100" min="0" max="50000" name="xmlsf_general_settings[limit]" id="xmlsf_sitemap_settings_limit" value="<?php echo esc_attr( $limit ); ?>" class="medium-text" />
-			<?php esc_html_e( 'URLs per sitemap', 'xml-sitemap-feed' ); ?>
 		</label>
 	</p>
 
--- views/admin/help-tab-post-types-general.php
+++ views/admin/help-tab-post-types-general.php
@@ -7,7 +7,7 @@
 
 ?>
 <p>
-	<strong><?php esc_html_e( 'URLs per sitemap', 'xml-sitemap-feed' ); ?></strong>
+	<strong><?php esc_html_e( 'Maximum posts per sitemap', 'xml-sitemap-feed' ); ?></strong>
 	<br />
 	<?php esc_html_e( 'The absolute maximum allowed is 50.000 per sitemap. Reduce this number if you experience errors or slow sitemaps.', 'xml-sitemap-feed' ); ?>
 	<?php //esc_html_e( '', 'xml-sitemap-feed' ); ?>
