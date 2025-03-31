5662ef58086508e1fc77c503597908a3bcfdc0b8
RavanH <ravanhagen@gmail.com>
2025-03-31 18:04:04 +0200

    extra warning about excluding pages

--- views/admin/help-tab-post-types-general.php
+++ views/admin/help-tab-post-types-general.php
@@ -9,11 +9,15 @@
 <p>
 	<strong><?php esc_html_e( 'Include these post types', 'xml-sitemap-feed' ); ?></strong>
 	<br />
+	<br />
 	<?php esc_html_e( 'Select the post types to include in the sitemap index. Select none to automatically include all public post types.', 'xml-sitemap-feed' ); ?>
+	<br />
+	<?php esc_html_e( 'Be aware: excluding the Pages sitemap, also means excluding the home aand blog pages!', 'xml-sitemap-feed' ); ?>
 </p>
 <?php if ( xmlsf()->sitemap->uses_core_server() ) : ?>
 <p>
 	<strong><?php esc_html_e( 'Maximum posts per sitemap', 'xml-sitemap-feed' ); ?></strong>
+	<br />
 	<br />
 	<?php esc_html_e( 'The absolute maximum allowed is 50.000 per sitemap. Reduce this number if you experience errors or slow sitemaps.', 'xml-sitemap-feed' ); ?>
 </p>
