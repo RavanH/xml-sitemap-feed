8e2a7a5d0e6dbbdda5e878227241e2120703c8e4
RavanH <ravanhagen@gmail.com>
2025-04-05 16:03:34 +0200

    use new method using_permalinks

--- views/admin/field-robots.php
+++ views/admin/field-robots.php
@@ -9,9 +9,9 @@
 <fieldset>
 	<legend class="screen-reader-text"><?php esc_html_e( 'Additional robots.txt rules', 'xml-sitemap-feed' ); ?></legend>
 
-<?php if ( ! $wp_rewrite->using_permalinks() ) { ?>
+<?php if ( ! xmlsf()->using_permalinks() ) { ?>
 	<p class="description">
-		<?php esc_html_e( 'This site is configured to use the Plain permalink structure.', 'xml-sitemap-feed' ); ?>
+		<?php printf( /* Translators: Permalinks */ esc_html__( 'Not available because of this site\'s %s settings.', 'xml-sitemap-feed' ), '<a href="' . esc_url( admin_url( 'options-permalink.php' ) ) . '">' . esc_html( translate( 'Permalinks' ) ) . '</a>' ); ?>
 		<?php esc_html_e( 'Either switch to another Permalink structure or create a static robots.txt file in the site root and place your rules there.', 'xml-sitemap-feed' ); ?>
 	</p>
 	<?php } elseif ( ! isset( $rules['robots\.txt$'] ) ) { ?>
--- views/admin/field-sitemap-name.php
+++ views/admin/field-sitemap-name.php
@@ -5,13 +5,20 @@
  * @package XML Sitemap & Google News
  */
 
+$using_permalinks = xmlsf()->using_permalinks();
 ?>
 <fieldset>
 	<legend class="screen-reader-text"><?php esc_html_e( 'XML Sitemap URL', 'xml-sitemap-feed' ); ?></legend>
 
-	<?php echo esc_url( trailingslashit( get_home_url() ) ); ?><input type="text" name="xmlsf_sitemap_name" id="xmlsf_sitemap_name" placeholder="sitemap" value="<?php echo esc_attr( $slug ); ?>" <?php disabled( apply_filters( 'xmlsf_advanced_enabled', false ), false ); ?>>.xml
+	<?php echo esc_url( trailingslashit( get_home_url() ) ); ?><input type="text" name="xmlsf_sitemap_name" id="xmlsf_sitemap_name" placeholder="sitemap" value="<?php echo esc_attr( $slug ); ?>" <?php disabled( ! apply_filters( 'xmlsf_advanced_enabled', false ) || ! $using_permalinks, true ); ?>>.xml
 	<p class="description" id="xmlsf-sitemap-name-description">
-		<?php esc_html__( 'Set an alternative name for the sitemap index.', 'xml-sitemap-feed' ); ?>
-		<?php apply_filters( 'xmlsf_advanced_enabled', false ) || printf( /* Translators: XML Sitemap Advanced */ esc_html__( 'Available in %s.', 'xml-sitemap-feed' ), '<a href="https://premium.status301.com/downloads/xml-sitemap-advanced/" target="_blank">' . esc_html__( 'XML Sitemap Advanced', 'xml-sitemap-feed' ) . '</a>' ); ?>
+		<?php esc_html_e( 'Set an alternative name for the sitemap index.', 'xml-sitemap-feed' ); ?>
+		<?php if ( ! $using_permalinks ) { ?>
+			<em>
+			<?php printf( /* Translators: Permalinks */ esc_html__( 'Not available because of this site\'s %s settings.', 'xml-sitemap-feed' ), '<a href="' . esc_url( admin_url( 'options-permalink.php' ) ) . '">' . esc_html( translate( 'Permalinks' ) ) . '</a>' ); ?>
+			</em>
+		<?php } else { ?>
+			<?php apply_filters( 'xmlsf_advanced_enabled', false ) || printf( /* Translators: XML Sitemap Advanced */ esc_html__( 'Available in %s.', 'xml-sitemap-feed' ), '<a href="https://premium.status301.com/downloads/xml-sitemap-advanced/" target="_blank">' . esc_html__( 'XML Sitemap Advanced', 'xml-sitemap-feed' ) . '</a>' ); ?>
+		<?php } ?>
 	</p>
 </fieldset>
