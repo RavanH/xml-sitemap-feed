9c28d48768f98c911568ea90732adc3c6886deec
RavanH <ravanhagen@gmail.com>
2025-04-27 20:05:29 +0200

    drop seo compatibility

--- inc/functions.php
+++ inc/functions.php
@@ -331,20 +331,6 @@ function plugin_compat() {
 	if ( in_array( 'xml-sitemaps-manager/xml-sitemaps-manager.php', $active_plugins, true ) ) {
 		\add_filter( 'plugins_loaded', array( __NAMESPACE__ . '\Compat\XMLSM', 'disable' ), 11 );
 	}
-
-	// Rankmath compatibility.
-	if ( in_array( 'seo-by-rank-math/rank-math.php', $active_plugins, true ) ) {
-		\add_filter( 'xmlsf_excluded', array( __NAMESPACE__ . '\Compat\Rank_Math', 'exclude_noindex' ), 10, 2 );
-		\add_filter( 'xmlsf_news_excluded', array( __NAMESPACE__ . '\Compat\Rank_Math', 'exclude_noindex' ), 10, 2 );
-		\add_filter( 'wp_sitemaps_posts_query_args', array( __NAMESPACE__ . '\Compat\Rank_Math', 'posts_query_args' ), 11 );
-	}
-
-	// Yoast SEO compatibility.
-	if ( in_array( 'wordpress-seo/wp-seo.php', $active_plugins, true ) ) {
-		\add_filter( 'xmlsf_excluded', array( __NAMESPACE__ . '\Compat\WordPress_SEO', 'exclude_noindex' ), 10, 2 );
-		\add_filter( 'xmlsf_news_excluded', array( __NAMESPACE__ . '\Compat\WordPress_SEO', 'exclude_noindex' ), 10, 2 );
-		\add_filter( 'wp_sitemaps_posts_query_args', array( __NAMESPACE__ . '\Compat\WordPress_SEO', 'posts_query_args' ), 11 );
-	}
 }
 
 /**
