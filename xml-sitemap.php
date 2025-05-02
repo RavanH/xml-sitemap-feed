f47a56191e500447c9fd2da25bebd250cd2b50d9
RavanH <ravanhagen@gmail.com>
2025-05-03 01:42:41 +0200

    anonymous autoloader

--- xml-sitemap.php
+++ xml-sitemap.php
@@ -146,27 +146,27 @@ register_activation_hook( __FILE__, 'xmlsf_activate' );
  *
  * @param string $class_name Namespaced class name.
  */
-function xmlsf_autoloader( $class_name ) {
-	// Bail if the class is not in our namespace.
-	if ( 0 !== strpos( $class_name, 'XMLSF\\' ) ) {
-		return;
+spl_autoload_register(
+	function ( $class_name ) {
+		// Bail if the class is not in our namespace.
+		if ( 0 !== strpos( $class_name, 'XMLSF\\' ) ) {
+			return;
+		}
+
+		// Build the filename and path.
+		$class_name = str_replace( 'XMLSF', 'inc', $class_name );
+		$class_name = strtolower( $class_name );
+		$path_array = explode( '\\', $class_name );
+		$class_name = array_pop( $path_array );
+		$class_name = str_replace( '_', '-', $class_name );
+		$file       = realpath( XMLSF_DIR ) . DIRECTORY_SEPARATOR . \implode( DIRECTORY_SEPARATOR, $path_array ) . DIRECTORY_SEPARATOR . 'class-' . $class_name . '.php';
+
+		// If the file exists for the class name, load it.
+		if ( file_exists( $file ) ) {
+			include_once $file;
+		}
 	}
-
-	// Build the filename and path.
-	$class_name = str_replace( 'XMLSF', 'inc', $class_name );
-	$class_name = strtolower( $class_name );
-	$path_array = explode( '\\', $class_name );
-	$class_name = array_pop( $path_array );
-	$class_name = str_replace( '_', '-', $class_name );
-	$file       = realpath( XMLSF_DIR ) . DIRECTORY_SEPARATOR . \implode( DIRECTORY_SEPARATOR, $path_array ) . DIRECTORY_SEPARATOR . 'class-' . $class_name . '.php';
-
-	// If the file exists for the class name, load it.
-	if ( file_exists( $file ) ) {
-		include_once $file;
-	}
-}
-
-spl_autoload_register( 'xmlsf_autoloader' );
+);
 
 /**
  * Deprecated, innit?
