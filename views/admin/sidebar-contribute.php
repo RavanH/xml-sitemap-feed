04115cfbf01f602a2237e3d01e07d9900573c983
RavanH <ravanhagen@gmail.com>
2025-10-14 23:09:48 +0200

    donate button

--- views/admin/sidebar-contribute.php
+++ views/admin/sidebar-contribute.php
@@ -8,26 +8,12 @@
 ?>
 <h3><span class="dashicons dashicons-thumbs-up"></span> <?php esc_html_e( 'Contribute', 'xml-sitemap-feed' ); ?></h3>
 <p>
-	<a target="_blank" href="https://www.paypal.com/donate/?hosted_button_id=5UVXZVN5HDKBS"
+	<a target="_blank" href="https://www.paypal.com/donate/?hosted_button_id=5UVXZVN5HDKBS" style="text-decoration:none;"
 		title="<?php printf( /* translators: Plugin name */ esc_html__( 'Donate to keep the free %s plugin development & support going!', 'xml-sitemap-feed' ), esc_html__( 'XML Sitemap & Google News', 'xml-sitemap-feed' ) ); ?>">
-		<img src="<?php trailingslashit( plugins_url( 'assets', XMLSF_BASENAME ) ); ?>donate.png" style="border:none;float:right;margin:4px 0 0 10px" width="92" height="26" />
-		<img src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" style="border:none;float:right;margin:4px 0 0 10px" width="92" height="26" />
+		<img src="<?php echo trailingslashit( plugins_url( 'assets', XMLSF_BASENAME ) ); ?>donate.png" style="border:none;float:right;margin:4px 0 0 10px; float: right;" width="92" height="92" />
+		<span style="display:block;float:right;width:fit-content;padding:4px 12px;background-color:#ffcc00;border:1px solid #555;border-radius:50px;color:black;text-decoration:none;font-weight:bold"><?php esc_html_e( 'Donate', 'xml-sitemap-feed' ); ?></span>
 	</a>
-	<div id="donate-button-container">
-<div id="donate-button"></div>
-<script src="https://www.paypalobjects.com/donate/sdk/donate-sdk.js" charset="UTF-8"></script>
-<script>
-PayPal.Donation.Button({
-env:'production',
-hosted_button_id:'5UVXZVN5HDKBS',
-image: {
-src:'https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif',
-alt:'Donate with PayPal button',
-title:'PayPal - The safer, easier way to pay online!',
-}
-}).render('#donate-button');
-</script>
-</div>
+
 	<?php
 	printf( /* translators: %1$s Review (linked to https://wordpress.org/support/plugin/xml-sitemap-feed/reviews/?filter=5#new-post) and %2$s Translating (linked to https://translate.wordpress.org/projects/wp-plugins/xml-sitemap-feed) */
 		esc_html__( 'If you would like to contribute and share with the rest of the WordPress community, please consider writing a quick %1$s or help out with %2$s!', 'xml-sitemap-feed' ),
