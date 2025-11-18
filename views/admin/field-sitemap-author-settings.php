d640ad20071b70ecaa80e939440a831f23f6f278
RavanH <ravanhagen@gmail.com>
2025-11-18 01:11:06 +0100

    fix litteral

--- views/admin/field-sitemap-author-settings.php
+++ views/admin/field-sitemap-author-settings.php
@@ -8,7 +8,7 @@
 ?>
 <fieldset id="xmlsf_author_settings">
 	<legend class="screen-reader-text">
-		<?php echo esc_html_e( translate( 'General' ) ); ?>
+		<?php echo esc_html( translate( 'General' ) );  // phpcs:ignore WordPress.WP.I18n.LowLevelTranslationFunction ?>
 	</legend>
 	<p>
 		<label>
@@ -23,7 +23,7 @@
 		<label>
 			<input type="checkbox" name="xmlsf_author_settings[include_empty]" id="xmlsf_author_include_empty" value="1"<?php checked( ! empty( $author_settings['include_empty'] ), true ); ?><?php disabled( apply_filters( 'xmlsf_advanced_enabled', false ), false ); ?> />
 			<?php esc_html_e( 'Include empty author archives.', 'xml-sitemap-feed' ); ?>
-			<?php apply_filters( 'xmlsf_advanced_enabled', false ) || printf( /* Translators: XML Sitemap Advanced */ esc_html__( 'Available in %s.', 'xml-sitemap-feed' ), '<a href="https://premium.status301.com/downloads/xml-sitemap-advanced/" target="_blank">' . esc_html__( 'XML Sitemap Advanced', 'xml-sitemap-feed' ) . '</a>' ); ?>
+			<?php apply_filters( 'xmlsf_advanced_enabled', false ) || printf( /* Translators: %s: XML Sitemap Advanced (with link) */ esc_html__( 'Available in %s.', 'xml-sitemap-feed' ), '<a href="https://premium.status301.com/downloads/xml-sitemap-advanced/" target="_blank">' . esc_html__( 'XML Sitemap Advanced', 'xml-sitemap-feed' ) . '</a>' ); ?>
 		</label>
 	</p>
 	<p>
