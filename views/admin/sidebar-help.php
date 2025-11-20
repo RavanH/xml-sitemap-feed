312de3f2b0cd8e2db8011ea2fba8928f3ac175ff
RavanH <ravanhagen@gmail.com>
2025-11-21 00:35:50 +0100

    KB in help section

--- views/admin/sidebar-help.php
+++ views/admin/sidebar-help.php
@@ -6,13 +6,22 @@
  */
 
 ?>
-<h3><span class="dashicons dashicons-sos"></span> <?php echo esc_html( translate( 'Help' ) ); ?></h3>
+<h3><span class="dashicons dashicons-sos"></span> <?php echo esc_html( translate( 'Help' ) ); // phpcs:ignore WordPress.WP.I18n.LowLevelTranslationFunction ?></h3>
 <p>
 	<?php
 	printf(
-		/* translators: %1$s Help tab, %2$s Support forum (linked to https://wordpress.org/support/plugin/xml-sitemap-feed) */
-		esc_html__( 'You can find instructions on the %1$s above. If you still have questions, please go to the %2$s.', 'xml-sitemap-feed' ),
-		'<strong>' . esc_html__( 'Help tab', 'xml-sitemap-feed' ) . '</strong>',
+		/* translators: %s: Help tab */
+		esc_html__( 'For help on the plugin options, open the %s above.', 'xml-sitemap-feed' ),
+		'<a href="#wpbody-content" onclick="javascript:jQuery(\'#contextual-help-link\').trigger(\'click\');">' . esc_html__( 'Help tab', 'xml-sitemap-feed' ) . '</a>'
+	);
+	printf(
+		/* translators: %s Knowledge Base (linked to https://premium.status301.com/knowledge-base/xml-sitemap-google-news/) */
+		esc_html__( 'Documentation on various related subjects can be found in our %s.', 'xml-sitemap-feed' ),
+		'<a href="https://premium.status301.com/knowledge-base/xml-sitemap-google-news/" target="_blank">' . esc_html__( 'Knowledge Base', 'xml-sitemap-feed' ) . '</a>'
+	);
+	printf(
+		/* translators: %s Support forum (linked to https://wordpress.org/support/plugin/xml-sitemap-feed) */
+		esc_html__( 'If you still have questions, please refer to the %s.', 'xml-sitemap-feed' ),
 		'<a href="https://wordpress.org/support/plugin/xml-sitemap-feed" target="_blank">' . esc_html__( 'Support forum', 'xml-sitemap-feed' ) . '</a>'
 	);
 	?>
