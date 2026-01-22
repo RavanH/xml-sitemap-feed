c7827e654f0afff1f8497eb3a0592ac849f78599
RavanH <ravanhagen@gmail.com>
2026-01-22 18:14:08 +0100

    connected status flag 2

--- views/admin/section-bwt-oauth-stage-2.php
+++ views/admin/section-bwt-oauth-stage-2.php
@@ -31,6 +31,9 @@ if ( \is_wp_error( $data ) ) {
 	</p>
 	<?php
 } else {
+	// Update option with 'connected' status.
+	\update_option( self::$option_group, array( 'bing_api_key' => self::$pw_placeholder, 'connected' => true ) );
+
 	// Display success message and redirect.
 	$redirect_url = \add_query_arg( array( 'page' => 'xmlsf', 'settings-updated' => 'true' ), \admin_url( 'options-general.php' ) );
 	?>
